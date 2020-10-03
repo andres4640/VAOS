7a46da13f0481faab33edca012961f7f3a1fe060
Camila <camilaf0509@gmail.com>
2020-10-03 12:19:15 -0500

    js

---
+++ js/signup_client.js
@@ -0,0 +1,182 @@
+$(document).ready(function () {
+    var ahora = new Date();
+    var dd = ahora.getDate();
+    var mm = ahora.getMonth()+1;
+    var yyyy = ahora.getFullYear()-18;
+    if(dd<10){
+        dd='0'+dd;
+    }
+    if(mm<10){
+        mm='0'+mm;
+    }
+
+    ahora = yyyy+"-"+mm+"-"+dd;
+    $("#fnacimiento").attr('max',ahora);
+}); 
+
+var validNombre = false;
+var validApe = false;
+var validUsername = false;
+var validEmail = false;
+var validPass = false;
+var validFecha = false;
+
+$("#nombres").on('blur', function(){
+    var nombres=$("#nombres").val();
+    if(nombres=="" || nombres==null){
+        $(this).removeClass("is-valid");
+        $(this).addClass("is-invalid");
+        validNombre=false;
+    }
+    else{
+        $(this).removeClass("is-invalid");
+        $(this).addClass("is-valid");
+        validNombre=true;
+    }
+    if($("#terminos").prop("checked") && validarTodo()){
+        $("#signup").removeAttr("disabled");
+    }
+    else{
+        $("#signup").attr("disabled",true);
+    }
+})
+
+$("#apellidos").on('blur', function(){
+    var apellidos=$("#apellidos").val();
+    if(apellidos=="" || apellidos==null){
+        $(this).removeClass("is-valid");
+        $(this).addClass("is-invalid");
+        validApe=false;
+    }
+    else{
+        $(this).removeClass("is-invalid");
+        $(this).addClass("is-valid");
+        validApe=true;
+    }
+    if($("#terminos").prop("checked") && validarTodo()){
+        $("#signup").removeAttr("disabled");
+    }
+    else{
+        $("#signup").attr("disabled",true);
+    }
+})
+
+$("#correo").on('blur', function(){
+    var correo=$("#correo").val();
+    emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i
+    if(emailRegex.test(correo)){
+        $(this).removeClass("is-invalid");
+        $(this).addClass("is-valid")
+        validEmail=true;
+    }
+    else{
+        $(this).removeClass("is-valid");
+        $(this).addClass("is-invalid");
+        validEmail=false;
+    }
+    if($("#terminos").prop("checked") && validarTodo()){
+        $("#signup").removeAttr("disabled");
+    }
+    else{
+        $("#signup").attr("disabled",true);
+    }
+})
+
+$("#username").on('blur', function(){
+    var username=$("#username").val();
+    if(username=="" || username==null || username.length < 8){
+        $(this).removeClass("is-valid");
+        $(this).addClass("is-invalid");
+        validUsername=false;
+    }
+    else{
+        $(this).removeClass("is-invalid");
+        $(this).addClass("is-valid");
+        validUsername=true;
+    }
+    if($("#terminos").prop("checked") && validarTodo()){
+        $("#signup").removeAttr("disabled");
+    }
+    else{
+        $("#signup").attr("disabled",true);
+    }
+})
+
+$("#password").on('blur', function(){
+    var password=$("#password").val();
+    if(password=="" || password==null){
+        $(this).removeClass("is-valid");
+        $(this).addClass("is-invalid")
+    }
+    else{
+        $(this).removeClass("is-invalid");
+        $(this).addClass("is-valid");
+    }
+    if($("#terminos").prop("checked") && validarTodo()){
+        $("#signup").removeAttr("disabled");
+    }
+    else{
+        $("#signup").attr("disabled",true);
+    }
+})
+
+$("#rpassword").on('blur', function(){
+    var password=$("#password").val();
+    var rpassword=$("#rpassword").val();
+    if(rpassword=="" || rpassword==null || rpassword!=password){
+        $(this).removeClass("is-valid");
+        $(this).addClass("is-invalid");
+        validPass=false;
+    }
+    else{
+        $(this).removeClass("is-invalid");
+        $(this).addClass("is-valid");
+        validPass=true;
+    }
+    if($("#terminos").prop("checked") && validarTodo()){
+        $("#signup").removeAttr("disabled");
+    }
+    else{
+        $("#signup").attr("disabled",true);
+    }
+})
+
+
+
+$("#fnacimiento").on('blur', function()
+{
+    var fecha = new Date($("#fnacimiento").val());
+    fecha.setDate(fecha.getDate()+1);
+    console.log(fecha);
+    if($("#fnacimiento").val()==null || $("#fnacimiento").val()=="")
+    {
+        validFecha=false;
+    }
+    else{
+        validFecha=true;
+    }
+    if($("#terminos").prop("checked") && validarTodo()){
+        $("#signup").removeAttr("disabled");
+    }
+    else{
+        $("#signup").attr("disabled",true);
+    }
+})
+
+function validarTodo()
+{
+    if(validEmail && validNombre && validPass && validApe && validUsername && validFecha)
+    {
+        return true;
+    }
+    return false;
+}
+
+$("#terminos").on("click", function(){
+    if($("#terminos").prop("checked") && validarTodo()){
+        $("#signup").removeAttr("disabled");
+    }
+    else{
+        $("#signup").attr("disabled",true);
+    }
+})
---
+++ js/signup_empresa.js
@@ -0,0 +1,144 @@
+var validNombre = false;
+var validRuc = false;
+var validTel = false;
+var validEmail = false;
+var validPass = false;
+
+
+$("#nombre-empresa").on('blur', function(){
+    var nombre=$("#nombre-empresa").val();
+    if(nombre=="" || nombre==null){
+        $(this).removeClass("is-valid");
+        $(this).addClass("is-invalid");
+        validNombre = false;
+    }
+    else{
+        $(this).removeClass("is-invalid");
+        $(this).addClass("is-valid");
+        validNombre = true;
+    }
+    if($("#terminos").prop("checked") && validarTodo()){
+        $("#signup").removeAttr("disabled");
+    }
+    else{
+        $("#signup").attr("disabled",true);
+    }
+})
+
+$("#ruc").on('blur', function(){
+    var ruc=$("#ruc").val();
+    if(ruc=="" || ruc==null){
+        $(this).removeClass("is-valid");
+        $(this).addClass("is-invalid");
+        validRuc = false;
+    }
+    else{
+        $(this).removeClass("is-invalid");
+        $(this).addClass("is-valid");
+        validRuc = true;
+     }
+     if($("#terminos").prop("checked") && validarTodo()){
+        $("#signup").removeAttr("disabled");
+    }
+    else{
+        $("#signup").attr("disabled",true);
+    }
+})
+
+$("#telefono").on('blur', function(){
+    var telefono=$("#telefono").val();
+    if(telefono=="" || telefono==null){
+        $(this).removeClass("is-valid");
+        $(this).addClass("is-invalid");
+        validTel = false;
+    }
+    else{
+        $(this).removeClass("is-invalid");
+        $(this).addClass("is-valid");
+        validTel = true;
+    }
+    if($("#terminos").prop("checked") && validarTodo()){
+        $("#signup").removeAttr("disabled");
+    }
+    else{
+        $("#signup").attr("disabled",true);
+    }
+})
+
+$("#correo").on('blur', function(){
+    var correo=$("#correo").val();
+    emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i
+    if(emailRegex.test(correo)){
+        $(this).removeClass("is-invalid");
+        $(this).addClass("is-valid")
+        validEmail=true;
+    }
+    else{
+        $(this).removeClass("is-valid");
+        $(this).addClass("is-invalid");
+        validEmail=false;
+    }
+    if($("#terminos").prop("checked") && validarTodo()){
+        $("#signup").removeAttr("disabled");
+    }
+    else{
+        $("#signup").attr("disabled",true);
+    }
+})
+
+$("#password").on('blur', function(){
+    var password=$("#password").val();
+    if(password=="" || password==null){
+        $(this).removeClass("is-valid");
+        $(this).addClass("is-invalid");
+    }
+    else{
+        $(this).removeClass("is-invalid");
+        $(this).addClass("is-valid");
+    }
+    if($("#terminos").prop("checked") && validarTodo()){
+        $("#signup").removeAttr("disabled");
+    }
+    else{
+        $("#signup").attr("disabled",true);
+    }
+})
+
+$("#rpassword").on('blur', function(){
+    var password=$("#password").val();
+    var rpassword=$("#rpassword").val();
+    if(rpassword=="" || rpassword==null || rpassword!=password){
+        $(this).removeClass("is-valid");
+        $(this).addClass("is-invalid")
+        validPass=false;
+    }
+    else{
+        $(this).removeClass("is-invalid");
+        $(this).addClass("is-valid");
+        validPass=true;
+    }
+    if($("#terminos").prop("checked") && validarTodo()){
+        $("#signup").removeAttr("disabled");
+    }
+    else{
+        $("#signup").attr("disabled",true);
+    }
+})
+
+function validarTodo()
+{
+    if(validEmail && validNombre && validPass && validRuc && validTel)
+    {
+        return true;
+    }
+    return false;
+}
+
+$("#terminos").on("click", function(){
+    if($("#terminos").prop("checked") && validarTodo()){
+        $("#signup").removeAttr("disabled");
+    }
+    else{
+        $("#signup").attr("disabled",true);
+    }
+})
