b745cec42c3422f3bd1115b5146f47cc68cbd942
Paola Rabanal <pao.araceli@gmail.com>
2020-10-10 13:40:35 -0500

    api mapa agregado

--- static/js/principal.js
+++ static/js/principal.js
@@ -1,3 +1,25 @@
+window.onload = init;
+
+function init() {
+    const map = new ol.Map({
+        view: new ol.View({
+            center: [-8574833.568028785, -1359643.0389284105],
+            zoom: 20,
+            maxZoom: 30,
+            minZoom: 4
+        }),
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM()
+            })
+        ],
+        target: 'mapa'
+    })
+
+    map.on('click', function(evt) {
+        console.log(evt.coordinate);
+    })
+}
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
     document.getElementById("mySidebar").style.width = "250px";
