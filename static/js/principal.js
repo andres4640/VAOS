23a1f0c1933580c9a77e145354298df34b77510d
Paola Rabanal <pao.araceli@gmail.com>
2020-11-21 13:54:51 -0500

    cambios mapa

--- static/js/principal.js
+++ static/js/principal.js
@@ -1,49 +1,13 @@
-/* Configuracion del mapa */
+window.onload = initMap;
 
-x = navigator.geolocation;
+let map;
 
-x.getCurrentPosition(success, failure);
-
-function success(position) {
-  var myLat = position.coords.latitude;
-  var myLong = position.coords.longitude;
-
-  var coords = new google.maps.LatLng(myLat,myLong);
-
-  var myStyles =[
-    {
-        featureType: "poi",
-        elementType: "labels",
-        stylers: [
-              { visibility: "off" }
-        ]
-    }
-  ];
-
-  var mapOptions = {
-    zoom: 16,
-    center: coords,
-    styles: myStyles
-
-  }
-
-  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
-  var market = new google.maps.Marker({
-    map:map,
-    position:coords,
-    //icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
+function initMap() {
+  map = new google.maps.Map(document.getElementById("mapa"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
   });
-
-
 }
-
-function failure() {
-}
-
-/* Fin de Configuracion del mapa */
-
-
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
     document.getElementById("mySidebar").style.width = "250px";
